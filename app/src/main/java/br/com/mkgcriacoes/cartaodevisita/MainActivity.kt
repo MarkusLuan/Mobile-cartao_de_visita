1ae0e594f750048efa7396f52fc4ae979c7725c0
Markus Luan <markus23_luan@hotmail.com>
2021-08-04 21:23:08 -0300

    Implementando VIewBinding

--- app/src/main/java/br/com/mkgcriacoes/cartaodevisita/MainActivity.kt
+++ app/src/main/java/br/com/mkgcriacoes/cartaodevisita/MainActivity.kt
@@ -2,10 +2,16 @@ package br.com.mkgcriacoes.cartaodevisita
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import br.com.mkgcriacoes.cartaodevisita.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binder by lazy {
+      ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binder.root)
     }
 }
