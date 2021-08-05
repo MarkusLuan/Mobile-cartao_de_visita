95dddc2784feaad393c8d52d0735799deb6d3fe0
Markus Luan <markus23_luan@hotmail.com>
2021-08-04 21:25:05 -0300

    Criando classes modelo

---
+++ app/src/main/java/br/com/mkgcriacoes/cartaodevisita/model/CartaoVisita.kt
@@ -0,0 +1,10 @@
+package br.com.mkgcriacoes.cartaodevisita.model
+
+class CartaoVisita {
+    var id: Long? = null
+    var nome: String? = null
+    var empresa: Empresa? = null
+    var telefone: String? = null
+    var email: String? = null
+    var cor: Int? = null
+}
---
+++ app/src/main/java/br/com/mkgcriacoes/cartaodevisita/model/Empresa.kt
@@ -0,0 +1,10 @@
+package br.com.mkgcriacoes.cartaodevisita.model
+
+import android.graphics.Bitmap
+
+class Empresa {
+    var id: Long? = null
+    var razaoSocial: String? = null
+    var cnpj: String? = null
+    var icon: Bitmap? = null
+}
